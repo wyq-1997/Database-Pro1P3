bb198c593d72fd6e4a7a1b1cb27a711d3eeba33c
Yuqing Wu <657981436@qq.com>
2022-04-15 00:49:40 -0400

    lazy

--- server.py
+++ server.py
@@ -187,7 +187,7 @@ def another():
 def add_movie():
   name = request.form['name']
   print (name)
-  cmd = 'INSERT INTO Movies(name) VALUES (:name1)'
+  cmd = 'INSERT INTO Movies(name) VALUES (:name1, :name1, :name1, :name1, :name1, :name1, :name1, :name1)'
   g.conn.execute(text(cmd), name1 = name)
   return redirect('/')
 
